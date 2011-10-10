9d34e0b7d2972d84d0670195e1c468cd26a909b7
Verboloz <v.vitovt@gmail.com>
2011-10-10 16:39:18 +0300

    The Game initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Game::Application.config.secret_token = 'b0016a9c8efbf6dce7bba35be29b6575fdf88cba59cc49cd61b65f8bcd884fbef119d6b44ab6b5f224080010183a76d77de27304d0d525636193879e031b5f95'
