655b8a7c61509694c851169cc4cdd9ce2765f363
Vit <v.vitovt@gmail.com>
2011-10-12 18:54:03 +0300

    fix :host for restore password mailer

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,6 @@ module Game
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.action_mailer.default_url_options = { :host => 'game.aprt.info' }
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,5 +46,4 @@ Game::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => 'localhost:3011' }
 end
