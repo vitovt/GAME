f8cedbd96307c1eb210a5cb7a9f0b3a87962d6b3
Vit <v.vitovt@gmail.com>
2011-10-12 16:52:22 +0300

    Added competitors users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
     # Alternatively, you can add an admin attribute flag
   def admin?
-    ["v.vitovt@gmail.com", "hisspouse.yourservice.com"].include?(email)
+    ["v.vitovt@gmail.com", "hisspouse@yourservice.com"].include?(email)
   end
 
+  def competitors?
+    ["vitovt@grusha.org.ua", "hisspouse2@yourservice.com"].include?(email)
+  end
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
