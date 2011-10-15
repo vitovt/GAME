39cc06702cddd5e4f1447e183aa86565f020fd2b
Vit <v.vitovt@gmail.com>
2011-10-15 11:47:34 +0300

    Username field added to devise auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111012133145) do
+ActiveRecord::Schema.define(:version => 20111015082701) do
 
   create_table "posts", :force => true do |t|
     t.date     "publishdate"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20111012133145) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
