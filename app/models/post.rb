cf3ff30778c86c7c1cc18c570a64decbcda9c8e8
Vit <v.vitovt@gmail.com>
2011-11-14 09:57:51 +0300

    Added weekday in human readable format

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ActiveRecord::Base
   def self.color
   	['000','080','440','ddf', 'ddd', 'dfd', 'dff', 'fdd', 'fdf', 'ffd', 'fff']
   end
+  def self.weekday
+  	['Вс','Пн','Вт','Ср', 'Чт', 'Пт', 'Сб']
+  end
 end
